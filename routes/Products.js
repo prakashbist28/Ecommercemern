383f278f662af5352a40afbeb74fca10889eb537
Prakash Bist <prakashcena540@gmail.com>
2024-05-14 13:49:20 +0530

    Update Products.js

--- routes/Products.js
+++ routes/Products.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProduct, fetchAllProducts, fetchProductById, updateProduct } = require('../Controller/Product');
+const { createProduct, fetchAllProducts, fetchProductById, updateProduct } = require('../controller/Product');
 
 
 const router  = express.Router();
